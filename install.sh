d8f8e818c6eb8c54741db42335ed0e6d1aca7979
vjosmann <vjekoslav.osmann@zalando.de>
2018-03-27 23:01:54 +0200

    Apache Benchmark.

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo apt-get -y install cloc # lines of code
 sudo apt-get -y install tree
 sudo apt-get -y install wget
 sudo apt-get -y install curl
+sudo apt-get -y install apache2-utils # ab - apache benchmark, e.g. ab -k -c 2 -n 10 www.example.com # -k to reuse connections
 
 wget -qO- https://get.docker.com/ | sh
 sudo usermod -aG docker vosmann
